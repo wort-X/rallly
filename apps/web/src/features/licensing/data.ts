d865767a0bf62f31ab8a5bd65ef949a910e9d934
Tim Fritzen <tim.fritzen@rwth-aachen.de>
2025-11-20 15:41:57 +0100

    Removed licensing restrictions.

--- apps/web/src/features/licensing/data.ts
+++ apps/web/src/features/licensing/data.ts
@@ -10,22 +10,32 @@ export function getInstanceLicense() {
 }
 
 export const loadInstanceLicense = cache(async () => {
-  const license = await getInstanceLicense();
-
-  if (!license) {
-    return null;
-  }
-
   return {
-    licenseKey: license.licenseKey,
-    licenseeName: license.licenseeName,
-    licenseeEmail: license.licenseeEmail,
-    issuedAt: license.issuedAt,
-    expiresAt: license.expiresAt,
-    seats:
-      (license.seats ?? license.type === "ENTERPRISE")
-        ? Number.POSITIVE_INFINITY
-        : 1,
-    type: license.type,
+    licenseKey: "license.licenseKey",
+    licenseeName: "license.licenseeName",
+    licenseeEmail: "license.licenseeEmail@example.com",
+    issuedAt: "",
+    expiresAt: "",
+    seats: Number.POSITIVE_INFINITY,
+    type: "ENTERPRISE",
   };
+
+  // const license = await getInstanceLicense();
+
+  // if (!license) {
+  //   return null;
+  // }
+
+  // return {
+  //   licenseKey: license.licenseKey,
+  //   licenseeName: license.licenseeName,
+  //   licenseeEmail: license.licenseeEmail,
+  //   issuedAt: license.issuedAt,
+  //   expiresAt: license.expiresAt,
+  //   seats:
+  //     (license.seats ?? license.type === "ENTERPRISE")
+  //       ? Number.POSITIVE_INFINITY
+  //       : 1,
+  //   type: license.type,
+  // };
 });
